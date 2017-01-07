3769f6b102da41d046c23e75d17037d0f231a956
Keith Kelly <keithkelly31@gmail.com>
2017-01-07 14:31:13 -0500

    Adding some template literals

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* jshint expr: true */
+
 !function() {
   'use strict';
 
@@ -13,13 +15,13 @@
   function Board() {
     this.openSquaresCount = 9;
     this.squares = [];
-  };
+  }
 
   function Game() {
     this.player1 = new Player(1, 'img/o.svg', 'box-filled-1', 'screen-win-one');
     this.player2 = new Player(2, 'img/x.svg', 'box-filled-2', 'screen-win-two');
     this.currentPlayer = this.player1;
-  };
+  }
 
   function GameUI() {
     this.body = document.querySelector('body');
@@ -35,7 +37,7 @@
     this.playerNumber = playerNumber;
     this.svg = svg;
     this.winClass = winClass;
-  };
+  }
 
   // Pass an index to the square so that each square can be tracked
   function Square(index) {
@@ -59,8 +61,8 @@
 
     // Show current players symbol when hovering over current unoccupied squares
     this.square.addEventListener('mouseenter', function() {
-      if(!self.isOccupied && !game.currentPlayer.isComputer) { 
-        self.square.style.backgroundImage = 'url(' + game.currentPlayer.svg + ')'; 
+      if(!self.isOccupied && !game.currentPlayer.isComputer) {
+        self.square.style.backgroundImage = 'url(' + game.currentPlayer.svg + ')';
       }
     }, false);
 
@@ -68,7 +70,7 @@
     this.square.addEventListener('mouseout', function() {
       if(!self.isOccupied) { self.square.style.backgroundImage = ''; }
     }, false);
-  };
+  }
 
   Board.prototype.loadNewBoard = function() {
     // Generate a new set of squares
@@ -84,7 +86,6 @@
     let immediateAction = false;
     let numbersCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     let maxNumber = numbersCount[0];
-    let maxNumberIndex = 0;
     let attackerWinConfigs = [];
 
     // Loop through win configurations
@@ -157,22 +158,22 @@
   Board.prototype.checkForWinner = function() {
     for(let i = 0; i < winConfigs.length; i++) {
       let counter = 0;
-      
+
       for(let j = 0; j < this.squares.length; j++) {
         const currentSquare = this.squares[j];
         if(currentSquare.occupyingPlayer === game.currentPlayer.playerNumber && winConfigs[i].includes(currentSquare.index)) { counter++; }
         if(counter === winConfigs[i].length) { return true; }
-      } 
+      }
     }
   };
 
   Board.prototype.progressGame = function() {
-    if(this.checkForWinner()) { 
-      gameUI.loadWinScreen(game.currentPlayer.winClass, game.currentPlayer.name); 
-    } else if(this.openSquaresCount === 0) { 
-      gameUI.loadWinScreen('screen-win-tie', null); 
-    } else { 
-      game.toggleCurrentPlayer(); 
+    if(this.checkForWinner()) {
+      gameUI.loadWinScreen(game.currentPlayer.winClass, game.currentPlayer.name);
+    } else if(this.openSquaresCount === 0) {
+      gameUI.loadWinScreen('screen-win-tie', null);
+    } else {
+      game.toggleCurrentPlayer();
     }
   };
 
@@ -196,16 +197,16 @@
     const self = this;
 
     // Build HTML
-    let html = '';
-    html += '<div class="screen screen-start">';
-    html += '<header>';
-    html += '<h1>Tic Tac Toe</h1>';
-    html += '<a href="#" class="button" id="one-player">One Player Game</a>';
-    html += '<a href="#" class="button" id="two-players">Two Player Game</a>';
-    html += '<div id="name-inputs"></div>';
-    html += '</header>';
-    html += '</div>';
-    this.body.innerHTML = html;
+    this.body.innerHTML = `
+      <div class="screen screen-start">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <a href="#" class="button" id="one-player">One Player Game</a>
+          <a href="#" class="button" id="two-players">Two Player Game</a>
+          <div id="name-inputs"></div>
+        </header>
+      </div>
+    `;
 
     // Set up player names when the single player or two player buttons are pressed
     document.getElementById('one-player').addEventListener('click', function() { self.playerNameSetup(1); }, false);
@@ -214,53 +215,52 @@
 
   GameUI.prototype.loadNewGameBoard = function() {
     // If there was a previous game, reset the game
-    if(board.openSquaresCount < 9) { 
+    if(board.openSquaresCount < 9) {
       game = new Game();
       board = new Board();
       game.player1.name = this.playerOneName;
       game.player2.name = this.playerTwoName;
       game.player2.isComputer = this.computerPlayer;
     }
-    
+
     // Build HTML
-    let html = '';
-    html += '<div class="board" id="board">';
-    html += '<header>';
-    html += '<h1>Tic Tac Toe</h1>';
-    html += '<ul>';
-    html += '<li class="players active" id="player1">' + game.player1.name + '</li>';
-    html += '<li class="players" id="player2">' + game.player2.name + '</li>';
-    html += '</ul>';
-    html += '</header>';
-    html += '<ul class="boxes"></ul>';
-    html += '</div>';
-    this.body.innerHTML = html;
+    this.body.innerHTML = `
+      <div class="board" id="board">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <ul>
+            <li class="players active" id="player1">${game.player1.name}</li>
+            <li class="players" id="player2">${game.player2.name}</li>
+          </ul>
+        </header>
+        <ul class="boxes"></ul>
+      </div>
+    `;
 
     board.loadNewBoard();
   };
 
   GameUI.prototype.loadWinScreen = function(winnerClass, winnerName) {
     const self = this;
-    
-    // Build HTML
-    let html = '';
-    html += '<div class="screen screen-win ' + winnerClass + '" id="finish">';
-    html += '<header>';
-    html += '<h1>Tic Tac Toe</h1>';
-    html += '<p class="message">';
+    let message;
 
     // Show the proper message depending on the outcome
     if(winnerClass === 'screen-win-tie') {
-      html += 'It\'s a Tie!';
+      message = 'It\'s a Tie!';
     } else {
-      html += winnerName + ' is the Winner!';
+      message = winnerName + ' is the Winner!';
     }
 
-    html += '</p>';
-    html += '<a href="#" class="button" id="start">New Game</a>';
-    html += '</header>';
-    html += '</div>';
-    this.body.innerHTML = html;
+    // Build HTML
+    this.body.innerHTML = `
+      <div class="screen screen-win ${winnerClass}" id="finish">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <p class="message">${message}</p>
+          <a href="#" class="button" id="start">New Game</a>
+        </header>
+      </div>
+    `;
 
     // Load new game board when the new game button is clicked
     document.getElementById('start').addEventListener('click', function() {
@@ -281,7 +281,7 @@
       html += '<label for="player2">Player 2 Name</label>';
       html += '<input type="text" id="player2">';
     }
-    html += '<button class="button" id="start-game" disabled>Start Game</button>'
+    html += '<button class="button" id="start-game" disabled>Start Game</button>';
     nameInputs.innerHTML = html;
 
     const button = document.getElementById('start-game');
@@ -325,7 +325,7 @@
     this.square.style.backgroundImage = '';
     this.square.style.cursor = 'default';
     board.openSquaresCount--;
-  }
+  };
 
   // Initiate app
   let game = new Game();
